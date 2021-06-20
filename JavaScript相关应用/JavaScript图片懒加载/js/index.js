9b88fd884fa4cbc648edc3c67545bb497873a8e5
814657780 <18037818812@163.com>
2021-06-20 22:37:20 +0800

    图片懒加载

--- JavaScript相关应用/JavaScript图片懒加载/js/index.js
+++ JavaScript相关应用/JavaScript图片懒加载/js/index.js
@@ -1,5 +1,6 @@
 const images = document.querySelectorAll('img')
 
+// 每次scroll的时候都会执行，如果图片很多的话性能很差。图片已经加载了，但是这个方法还是一直在执行。
 window.addEventListener('scroll',(e)=>{
       images.forEach(image=>{
           console.log('加载图片');
